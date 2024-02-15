bcebd686f9729a4d596b33bf0e84df0b443ff2ef
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-02-15 13:16:02 +0400

    Update heightweight.java

--- heightweight.java
+++ heightweight.java
@@ -1,22 +1,20 @@
 import java.util.Scanner;
-public class heightweight{
-	public static void main (String[] arg){
-		Scanner sc=new Scanner(System.in);
-    		System.out.println("Enter height");
-    		int d=sc.nextInt();
-    		System.out.println("Enter weight");
-    		int f=sc.nextInt();
-    
-    		int x=d-f;
-    
-   		if (x==100){
-      			System.out.println("Normal weight");
-    		}
-      		else if (x>100){
-       			System.out.println("Underweight  " + ((d-f)-100)+ "kq");
-      		}
-      		else if (x<100){
-        		System.out.println("Overweight  " + (100-(d-f))+ "kq");
-      		}
-  	}
+public class heightweight {
+  public static void main(String[] arg) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter height");
+    int d = sc.nextInt();
+    System.out.println("Enter weight");
+    int f = sc.nextInt();
+
+    int x = d - f;
+
+    if (x == 100) {
+      System.out.println("Normal weight");
+    } else if (x > 100) {
+      System.out.println("Underweight  " + ((d - f) - 100) + "kq");
+    } else if (x < 100) {
+      System.out.println("Overweight  " + (100 - (d - f)) + "kq");
+    }
+  }
 }
